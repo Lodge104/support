a5010a6f591753eed479cafb81fe719a1b7233c8
Nicholas Anderson <nickanderson1998@gmail.com>
2016-08-24 08:53:48 -0400

    Merger begin

---
+++ include/staff/templates/merge-ticket.tmpl.php
@@ -0,0 +1,7 @@
+<h3><i class="icon-copy"></i> <?php echo __('Merge Ticket'); ?></i></h3>
+<b><a class="close" href="#"><i class="icon-remove-circle"></i></a></b>
+<hr/><?php echo __(
+); ?>
+<br/>
+<br/>
+<form method="post" action="<?php echo $info['action']; ?>">
