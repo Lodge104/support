c81b57f2850b4f78870fe0c9df8dfa5c2d120df8
Nicholas Anderson <nickanderson1998@gmail.com>
2016-08-26 15:29:41 -0400

    GA

--- include/client/footer.inc.php
+++ include/client/footer.inc.php
@@ -3,6 +3,16 @@
     <div id="footer">
 <div class="container"> <div class="row">
     <div class="fwidgets">
+            <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-37461006-10', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
         <div class="col-md-4">
             <h2> Reach Us </h2>
@@ -34,10 +44,7 @@ Occoneechee Council - BSA<br>
                     <li> <a href="twitter.com/lodge104"> <i class="icon-twitter"></i> </a> </li>
                     <li> <a href="instagram.com/lodge104"> <i class="icon-instagram"></i> </a> </li>                        
                     <li> <a href="youtube.com/oalodge104"> <i class="icon-youtube"></i> </a> </li>
-<<<<<<< HEAD
-=======
                     <li> <a href="snapchat.com/add/lodge104"> <i class="icon-snapchat"></i> </a> </li>
->>>>>>> master
                 </ul>
             </div>            
 
