a1c248d8287ce7334d664e29fbaee8266ebc60a3
Nicholas Anderson <nickanderson1998@gmail.com>
2023-04-27 16:08:05 -0400

    /include/ajax.*

--- include/ajax.email.php
+++ include/ajax.email.php
@@ -25,7 +25,6 @@ class EmailAjaxAPI extends AjaxController {
         $info = $errors = [];
         if ($_POST && $account->saveAuth($auth, $form, $errors)) {
             if ($account->isOAuthAuth()
-                    && $account->isEnabled()
                     && $account->shouldAuthorize()) {
                  Http::response(201, JsonDataEncoder::encode([
                              'redirect' => sprintf('emails.php?id=%d&do=autho&bk=%s',
@@ -39,5 +38,22 @@ class EmailAjaxAPI extends AjaxController {
         list($authtype, $provider) = explode(':', $auth);
         $template = sprintf('email-%sauth.tmpl.php', $authtype);
         include INCLUDE_DIR . "staff/templates/$template";
+    }
+
+    /*
+     * Delete the OAuth2 token
+     */
+    function deleteToken($id, $type) {
+        // Check to make sure the email exists
+        if (!($email=Email::lookup($id)))
+            Http::response(404, 'Unknown Email');
+        // Get the authentication account
+        if (!($account=$email->getAuthAccount($type)))
+            Http::response(404, 'Unknown Authentication Type');
+        // Destory the account config which will delete the token
+        if ($account->destroyConfig())
+            Http::response(201, __('Token Deleted Successfully.'));
+        // Couldn't delete the Token
+        Http::response(404, 'Unable to delete token.');
     }
 }
