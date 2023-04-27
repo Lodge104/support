e8280375eeefa6b949ffa2106537f16859e4c932
Nicholas Anderson <nickanderson1998@gmail.com>
2023-04-27 16:04:02 -0400

    /setup

---
+++ setup/inc/footer.inc.php
@@ -0,0 +1,12 @@
+
+            <div class="clear"></div>
+        </div> <!-- content -->
+    </div> <!-- wizard -->
+    <div id="footer" class="centered">Copyright &copy; <?php echo date('Y'); ?> <a target="_blank" href="https://osticket.com">osTicket.com</a></div>
+
+    <script type="text/javascript" src="../js/jquery-3.6.3.min.js?ca95150"></script>
+    <script type="text/javascript" src="../js/jstz.min.js?ca95150"></script>
+    <script type="text/javascript" src="js/setup.js?ca95150"></script>
+    <script type="text/javascript" src="js/tips.js?ca95150"></script>
+</body>
+</html>
